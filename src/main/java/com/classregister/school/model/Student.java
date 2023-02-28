6cea2242eb8d1f628f76dc25f6d144437b91c0dc
mhimf001 <flavianmachimbirike@gmail.com>
2023-02-28 17:26:31 +0200

    DDL fixes.

--- src/main/java/com/classregister/school/model/Student.java
+++ src/main/java/com/classregister/school/model/Student.java
@@ -1,36 +1,44 @@
 package com.classregister.school.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data   // removes boiler code by implicitly inserting getters, setters  and constructors
-@Entity() //maps our java  object  to the database
-@NoArgsConstructor // no args constructor
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity(name = "Student")
+@Table(name = "student")
 public class Student {
-@Id
-@SequenceGenerator( name = "id-sequence"
-, sequenceName = "id-sequence" ,allocationSize = 1)
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
 
-@GeneratedValue(strategy = GenerationType.SEQUENCE,
-generator ="id-sequence")
-@Column(name="national_id",nullable = false , updatable = false, unique = true)
-private Integer id;
-    @Column(name="student_first_name", columnDefinition = "TEXT")
+    @Column(name="student_first_name")
     private String studentFirstName;
-    @Column(name="student_surname",columnDefinition = "TEXT")
+    @Column(name="student_surname")
     private String studentSurname;
-    @Column(name="student_class")
     private Integer classs;
-
-
-
-    @Column(name="national_id",nullable = false , updatable = false)
+    private Integer institution;
+    @Column(name="national_id")
     private String nationalId;
 
+//    @ManyToOne
+//    @JoinColumn(name="institution")
 
+//    @ManyToOne
+//    @JoinColumn(name = "classs",insertable = false, updatable = false)
+//    private Classes classes;
+
+
+//    @OneToMany(fetch=FetchType.LAZY)
+//    private Set<Receipt> receipts = new HashSet<>();
+//    @OneToMany(fetch=FetchType.LAZY)
+//    private Set<Invoice> invoices = new HashSet<>();
 }
--- src/main/java/com/classregister/school/repository/StudentRepository.java
+++ src/main/java/com/classregister/school/repository/StudentRepository.java
@@ -1,15 +1,16 @@
 package com.classregister.school.repository;
 
 
-/*
-//@Repository
+import com.classregister.school.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public interface StudentRepository extends JpaRepository<Student,Integer> {
     @Query(value="select * from student where institution=:institutionId", nativeQuery = true)
     List<Student> getAllStudents(Integer institutionId);
-
-
-
-
-
 }
-     */
+
